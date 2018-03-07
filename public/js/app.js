68ec520fc1b0dad69c7185204a9f9bf4560b9bb6
David Larsson <manzey@hotmail.se>
2018-03-07 18:10:00 +0100

    Testing notifications via webhook on live

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,29 @@
-window.onload=function() {    
+window.onload=function() {  
+    
     document.getElementById("issues").addEventListener("click",() => {
-        alert('hej')
+        notify('Issue opened!', 'body.issue.html_url', 'body.issue.title', 'body.issue.user.login')       
     })
 
-    // 2018-03-06T15:24:50Z
+
 
     let socket = io.connect()
     socket.on('incoming', (event) => {
         let body = event.body
-        if (body.action == "opened") {
+        if (body.action == "opened" || body.action == "reopened") {
             let issueList = document.getElementById('issueList')
-            let template = document.getElementById('issueTemp')
-            let clone = template.content.cloneNode(true)
-            clone.getElementById('issueLink').href = body.issue.html_url
-            clone.getElementById('issueLink').innerHTML = body.issue.user.login
-            clone.getElementById('issueTitle').innerHTML = body.issue.title
-            clone.getElementById('issueCreated').innerHTML = moment(body.issue.created_at).format('YYYY-MM-DD, h:mm a')
-            clone.getElementById('issueUpdated').innerHTML = moment(body.issue.updated_at).format('YYYY-MM-DD, h:mm a')
-            issueList.prepend(clone)        
+            let issueTemp = document.getElementById('issueTemp')
+            let issueClone = issueTemp.content.cloneNode(true)
+            issueClone.getElementById('issueLink').href = body.issue.html_url
+            issueClone.getElementById('issueLink').innerHTML = body.issue.user.login
+            issueClone.getElementById('issueTitle').innerHTML = body.issue.title
+            issueClone.getElementById('issueCreated').innerHTML = moment(body.issue.created_at).format('YYYY-MM-DD, h:mm a')
+            issueClone.getElementById('issueUpdated').innerHTML = moment(body.issue.updated_at).format('YYYY-MM-DD, h:mm a')
+            issueList.prepend(issueClone)
+            if (body.action == "opened")
+            notify('Issue opened!', body.issue.html_url, body.issue.title, body.issue.user.login)
+            
+            if (body.action == "reopened")
+            notify('Issue reopened!', body.issue.html_url, body.issue.title, body.issue.user.login)
         } else if (body.action == "closed") {
             let parent = document.getElementById('issueList')
             let children = parent.getElementsByTagName('div')
@@ -28,6 +34,22 @@ window.onload=function() {
                     }
                 }
             }
+            notify('Issue closed!', body.issue.html_url, body.issue.title, body.issue.user.login)
+        } else if (body.action == "created") {
+            notify('New comment!', body.issue.html_url, body.issue.title, body.issue.user.login)
+        } else if (body.action == "deleted") {
+            notify('Comment deleted!', body.issue.html_url, body.issue.title, body.issue.user.login)
         }
     })
+}
+
+function notify(action, titleLink, titleBody, user) {
+    let notificationList = document.getElementById('notificationList')
+    let notifyTemp = document.getElementById('notifyTemp')
+    let notifyClone = notifyTemp.content.cloneNode(true)
+    notifyClone.getElementById('notifyAction').innerHTML = action
+    notifyClone.getElementById('notifyTitle').href = titleLink
+    notifyClone.getElementById('notifyTitle').innerHTML = titleBody
+    notifyClone.getElementById('notifyUser').innerHTML = user     
+    notificationList.prepend(notifyClone)
 }
