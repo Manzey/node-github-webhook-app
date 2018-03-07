c2f2ad39c87bf6228380dbd70ee618ec9294bb87
David Larsson <manzey@hotmail.se>
2018-03-07 15:05:33 +0100

    Append new issue when one is created

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,15 @@ window.onload=function() {
             console.log(body.issue.title)
             console.log(body.issue.created_at)
             console.log(body.issue.updated_at)
+            let issueList = document.getElementById('issueList')
+            let template = document.getElementById('issueTemp')
+            let clone = template.content.cloneNode(true)
+            issueList.appendChild(clone)
+            clone.getElementById('issueLink').href = body.issue.url
+            clone.getElementById('issueLink').innerHTML = body.issue.user.login
+            clone.getElementById('issueTitle').innerHTML = body.issue.title
+            clone.getElementById('issueCreated').innerHTML = body.issue.created_at
+            clone.getElementById('issueUpdated').innerHTML = body.issue.updated_at
         }
     })
 }
